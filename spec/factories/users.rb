d6493da6675fedc6e17b6f47ac020c678288b4cf
ayakasam <ayaka.182ms2k0123@gmail.com>
2024-07-17 18:28:05 +0900

    ストロングパラメーターとFactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
     user_name = Gimei.name
-    nickname {user_name}
-    email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    lastname {user_name.last }
-    firstname {user_name.first}
-    lastname_kana {user_name.last.katakana}
-    firstname_kana {user_name.first.katakana}
-    birthday {Faker::Date.birthday}
+    nickname { user_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) + '1a' }
+    password_confirmation { password }
+    lastname { user_name.last }
+    firstname { user_name.first }
+    lastname_kana { user_name.last.katakana }
+    firstname_kana { user_name.first.katakana }
+    birthday { Faker::Date.birthday }
   end
 end
