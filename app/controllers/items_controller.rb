dd6896713f204994c22a0273b95a0ba89a2d8dd5
ayakasam <ayaka.182ms2k0123@gmail.com>
2024-08-12 22:44:33 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_signin, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -34,6 +34,15 @@ class ItemsController < ApplicationController
       redirect_to item_path(params[:id])
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @item.user == current_user
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(params[:id])
     end
   end
 
