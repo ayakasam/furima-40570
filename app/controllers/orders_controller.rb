e838d7ab4be927dc291452be588cbf2183912c09
ayakasam <ayaka.182ms2k0123@gmail.com>
2024-08-20 15:57:15 +0900

    Formオブジェクト修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
-    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+    @orderaddress = OrderAddress.new
+  end
+
+  def create
+    @orderaddress = OrderAddress.new(order_params)
+    if @orderaddress.valid?
+      @orderaddress.save(params, current_user.id)
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postalcode, :prefecture, :municipalities, :street_address, :building_name,
+                                          :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,21 +1,21 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postalcode, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number,
-                :token
+  attr_accessor :item_id, :user_id, :postalcode, :prefecture, :municipalities, :street_address, :building_name, :phone_number
 
   with_options presence: true do
-    validates :user_id, :item_id, :token
+    validates :user_id
+    validates :item_id
     validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
-    validates :municipalities, :street_address
+    validates :municipalities
+    validates :street_address
     with_options length: { minimum: 10 } do
       validates :phone_number, format: { with: /\A[0-9]\z/, message: 'is invalid. Input only number' }
     end
   end
-
   def save
-    order = Order.create(user_id:, item_id:)
-    Address.create(postalcode:, prefecture:, city:, house_number:,
-                   building_name:, donation_id: order.id)
+    order = Order.create(item_id: item_id, user_id: user_id) # rubocop:disable Style/HashSyntax
+    Address.create(postalcode: postalcode, prefecture: prefecture, municipalities: municipalities, # rubocop:disable Style/HashSyntax
+                   street_address: street_address, building_name: building_name, phone_number: phone_number, order_id: order.id) # rubocop:disable Style/HashSyntax
   end
 end
