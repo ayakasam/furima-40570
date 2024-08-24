ba360656fbdfb6d5dec0ae4aed6a928834ee78dd
ayakasam <ayaka.182ms2k0123@gmail.com>
2024-08-24 19:41:03 +0900

    order_addressの単体テストコード

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,7 +10,9 @@ class OrderAddress
     validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :municipalities
     validates :street_address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
+    with_options format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only number' } do
+        validates :phone_number, length: { minimum: 10, maximum: 11, message: "is too short"}
+    end
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :order_address do
+    token {"tok_abcdefghijk00000000000000000"}
     postalcode { Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4) } # rubocop:disable Style/StringConcatenation
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     municipalities { Faker::Address.city }
     street_address { Faker::Address.street_address }
     building_name { Faker::Company.name }
     phone_number { Faker::Number.leading_zero_number(digits: 11) }
-    association :item
-    association :user
+    association :user_id
+    association :item_id
   end
 end
